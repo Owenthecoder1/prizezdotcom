6b88034d5cb4df740c632b240aebdff0dbe6bde3
Owenthecoder1 <71456832+Owenthecoder1@users.noreply.github.com>
2022-10-02 12:24:28 -0700

    rick roll

---
+++ index.js
@@ -0,0 +1,18 @@
+var achievements = document.getElementById("achievements")
+var membership = document.getElementById("membership")
+
+function achievementsRun (){
+	alert("To access your achievements you need to have a membership")
+	membershipRun()
+}
+
+function membershipRun (){
+	var membership = prompt("There are 3 levels of memberships with different perks. Normal: Access to achievements. Gold: Access to achievements and 2 free prizes. Diamond: Access to achievements with 50% of the requirements and 5 free prizes. Please enter Normal/Gold/Diamond")
+	if(membership == "Normal" || membership == "Gold" || membership == "Diamond"){
+		prompt("Please enter your credit/debit card ID")
+		document.body.innerHTML = "Insufficient Funds <span style = 'color:white'><a href='https://www.youtube.com/watch?v=iik25wqIuFo'>Right?</a></span>"
+	}
+}
+
+achievements.addEventListener("click", achievementsRun)
+membership.addEventListener("click", membershipRun)

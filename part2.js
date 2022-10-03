917c4c13d2e6baeaf36572ed461c7e7740b2538c
Owenthecoder1 <71456832+Owenthecoder1@users.noreply.github.com>
2022-10-02 17:52:33 -0700

    rikrol lie

--- part2.js
+++ part2.js
@@ -1,7 +1,13 @@
 var rickroll = document.getElementById("begin")
+var asterisk = document.getElementByTagName("span")
 
 function RICKROLLEDLOL (){
 	window.location.href = "https://www.youtube.com/watch?v=iik25wqIuFo";
 }
 
+function asteriskRun (){
+	alert("I LIKE TRAINS")
+}
+
 rickroll.addEventListener("click",RICKROLLEDLOL)
+asterisk.addEventListener("click",asteriskRun)

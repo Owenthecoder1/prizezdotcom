44d0560c89d441081b30d414ad11737b60531568
Owenthecoder1 <71456832+Owenthecoder1@users.noreply.github.com>
2022-10-02 17:37:14 -0700

    rickroll

---
+++ part2.js
@@ -0,0 +1,7 @@
+var rickroll = document.getElementById("begin")
+
+function RICKROLLEDLOL (){
+	window.location.href = "https://www.youtube.com/watch?v=iik25wqIuFo";
+}
+
+rickroll.addEventListener("click",RICKROLLEDLOL)

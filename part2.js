f380304d3cdd45ba9fb372436499f109c3a5846d
Owenthecoder1 <71456832+Owenthecoder1@users.noreply.github.com>
2022-10-02 17:58:57 -0700

    elements

--- part2.js
+++ part2.js
@@ -1,5 +1,5 @@
 var rickroll = document.getElementById("begin")
-var asterisk = document.getElementByTagName("span")
+var asterisk = document.getElementsByTagName("span")
 
 function RICKROLLEDLOL (){
 	window.location.href = "https://www.youtube.com/watch?v=iik25wqIuFo";
